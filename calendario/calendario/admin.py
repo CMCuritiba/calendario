71aaf0568eda200733ed43c2271e136e7a8ebe0a
aleodoni <aleodoni@gmail.com>
2019-02-08 10:08:42 -0200

    filtro para mostrar itens cancelados. closes #111

--- calendario/calendario/admin.py
+++ calendario/calendario/admin.py
@@ -2,7 +2,32 @@
 from __future__ import unicode_literals
 from django.contrib import admin
 
-from .models import Local, Evento
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from django.utils.translation import ugettext, ugettext_lazy as _
 
+from .models import Local, Evento
+from autentica.models import User as Usuario
+
+class MyUserAdmin(UserAdmin):
+    def get_fieldsets(self, request, obj=None):
+        if not obj:
+            return self.add_fieldsets
+
+        if request.user.is_superuser:
+            perm_fields = ('is_active', 'is_staff', 'is_superuser',
+                           'groups', 'user_permissions')
+        else:
+            # modify these to suit the fields you want your
+            # staff user to be able to edit
+            perm_fields = ('is_active', 'is_staff')
+
+        return [(None, {'fields': ('username', 'password')}),
+                (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
+                (_('Permissions'), {'fields': perm_fields}),
+                (_('Important dates'), {'fields': ('last_login', 'date_joined')})]
+
+admin.site.unregister(Usuario)
+admin.site.register(Usuario, MyUserAdmin)
 admin.site.register(Local)
 admin.site.register(Evento)
